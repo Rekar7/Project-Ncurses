919c5b13fb4bb9f9013c58416154514c7b475ff7
Rekar7 <wojtekbinczyk2003@gmail.com>
2023-11-23 21:17:57 +0100

    bordery

--- Project/main.cpp
+++ Project/main.cpp
@@ -7,8 +7,13 @@ int main()
 	//inicjalizuje ekran
 	//czyœci ekran
 	initscr();
+	cbreak();	//jak wciœniesz ctrl+c to program siê zamknie dzia³a defaultowo
 
-	int height=10, width=20, start_y=10, start_x=10;
+	//raw();		//raw input jak wciœniesz ctrl+c to ctrl te¿ siê wyœwietli
+
+	noecho();	//input nie pokazuje siê na ekranie
+
+	int height=30, width=120, start_y=0, start_x=0;
 
 	WINDOW* win=newwin(height, width, start_y, start_x);	//tworzy nowe okno // w nucruses zawsze w kordach najpierw jest
 							//y, a potem x
@@ -17,7 +22,11 @@ int main()
 	refresh();	//odœwie¿yæ wszystko, ¿eby ncurses wiedzia³, ¿e zrobiliœmy okono
 
 
-	
+
+	//pozwala na wiêksz¹ customizacje bordera boxa
+	//wborder(win, left, right, top, bottom, corner1, corner2, corner3, corner4);
+
+
 	box(win, 0, 0);	// robi kwadrat wokó³ okna
 	mvwprintw(win, 1, 1, "szczepan");	//rysuje w oknie
 	wrefresh(win);	//odœwie¿amy okno
