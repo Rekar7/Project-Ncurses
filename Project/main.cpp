01db90ae956054307b9547f4bd84b6ac1bd3e940
Rekar7 <wojtekbinczyk2003@gmail.com>
2023-11-23 20:34:22 +0100

    test

--- Project/main.cpp
+++ Project/main.cpp
@@ -14,6 +14,14 @@ int main()
 	//odœwie¿a ekran
 	refresh();
 
+	//przesuwa kursor do podanych kordów i na tych kordach s¹ pisane rzeczy  
+
+	move(10, 20); // move(y, x);
+
+	int c = getch();
+
+	printw("%d", c); //wyœwielta zmienn¹ c
+
 	//zwraca naciœniêty przycisk
 	getch();
 
